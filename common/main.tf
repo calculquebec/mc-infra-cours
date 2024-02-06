271005124a7719caa78707d0b93d2f1ed7eaa1a1
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-02-06 16:45:40 -0500

    Create common/main.tf

---
+++ common/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_version = ">= 1.4.0"
+}
+
+variable "pool" {
+  description = "Slurm pool of compute nodes"
+  default = []
+}
+
+variable "TFC_WORKSPACE_NAME" { type = string }
+variable "token_hieradata" {}
+variable "credentials_hieradata" { default= "" }
+
+data "tfe_workspace" "current" {
+  name         = var.TFC_WORKSPACE_NAME
+  organization = "CalculQuebec"
+}
+
+locals {
+  hieradata = yamlencode(merge(
+    var.token_hieradata,
+    var.credentials_hieradata,
+    {
+      "profile::slurm::controller::tfe_workspace" = data.tfe_workspace.current.id
+    },
+    yamldecode(file("../common/config.yaml"))
+    yamldecode(file("config.yaml"))
+  ))
+}
+
+module "openstack" {
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=13.3.1"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "13.3.1"
+
+  cluster_name = local.name
+  domain       = "calculquebec.cloud"
+  image        = "Rocky-8"
+
+  instances = local.instances
+
+  # var.pool is managed by Slurm through Terraform REST API.
+  # To let Slurm manage a type of nodes, add "pool" to its tag list.
+  # When using Terraform CLI, this parameter is ignored.
+  # Refer to Magic Castle Documentation - Enable Magic Castle Autoscaling
+  pool = var.pool
+
+  volumes = local.volumes
+
+  generate_ssh_key = true
+  public_keys = compact(concat(split("\n", file("../common/sshkeys.pub")), ))
+
+  nb_users = 0
+  # Shared password, randomly chosen if blank
+  guest_passwd = ""
+
+  hieradata = local.hieradata
+}
+
+output "accounts" {
+  value = module.openstack.accounts
+}
+
+output "public_ip" {
+  value = module.openstack.public_ip
+}
+
+# Uncomment to register your domain name with CloudFlare
+module "dns" {
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=13.3.1"
+  bastions         = module.openstack.bastions
+  name             = module.openstack.cluster_name
+  domain           = module.openstack.domain
+  public_instances = module.openstack.public_instances
+  ssh_private_key  = module.openstack.ssh_private_key
+  sudoer_username  = module.openstack.accounts.sudoer.username
+}
+
+output "hostnames" {
+  value = module.dns.hostnames
+}
