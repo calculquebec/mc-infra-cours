bf4b121d8061ff14bd57ce9205698bdf67bab4b3
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-02-07 11:06:13 -0500

    Update custom.tf

--- ift6095/custom.tf
+++ ift6095/custom.tf
@@ -1,10 +1,11 @@
 locals {
-  custom_home_size = 100
-  custom_project_size = 500
-  custom_scratch_size = 50
-
-  custom_ngpupool = 10
-  custom_ngpu = 1
+  custom {
+    ngpu = 1
+    ngpupool = 10
+    home_size = 100
+    project_size = 500
+    scratch_size = 50
+  }
 
   name = "ift6095"
 }
