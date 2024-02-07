3a97b151851a60c8055c1f222b09637ed458c0e7
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-02-07 10:15:53 -0500

    make instances and volumes customizable easier

--- common/main.tf
+++ common/main.tf
@@ -9,6 +9,7 @@ variable "pool" {
 
 variable "TFC_WORKSPACE_NAME" { type = string }
 variable "token_hieradata" {}
+variable "cloud_name" { type = string }
 
 data "tfe_workspace" "current" {
   name         = var.TFC_WORKSPACE_NAME
@@ -16,6 +17,52 @@ data "tfe_workspace" "current" {
 }
 
 locals {
+  image_cpu = local.custom_image_cpu ? local.custom_image_cpu : "snapshot-cpunode-2024.1"
+  image_gpu = local.custom_image_gpu ? local.custom_image_gpu : "snapshot-gpunode-2024.1"
+  ncpu = local.custom_ncpu ? local.custom_ncpu : 0
+  ngpu = local.custom_gcpu ? local.custom_gcpu : 0
+  ncpupool = local.custom_ncpupool ? local.custom_ncpupool : 2
+  ngpupool = local.custom_gcpupool ? local.custom_gcpupool : 2
+
+  default_instances_map = {
+    arbutus = {
+        mgmt   = { type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
+        login  = { type = "p4-6gb-avx2", tags = ["login", "public", "proxy"], count = 1 }
+        nodecpu   = { type = "c8-30gb-186-avx2", tags = ["node"], count = local.ncpu, image=local.image_gpu }
+        nodegpu   = { type = "g1-8gb-c4-22gb", tags = ["node"], count = local.ngpu, image=local.image_gpu }
+        nodepoolgpu   = { type = "g1-8gb-c4-22gb", tags = ["node", "pool"], count = local.ngpupool, image=local.image_gpu }
+        nodepoolcpu   = { type = "c8-30gb-186-avx2", tags = ["node", "pool"], count = local.ncpupool, image=local.image_gpu }
+    }
+    beluga = {
+        mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
+        login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1 }
+        nodecpu   = { type = "c8-60gb", tags = ["node"], count = local.ncpu, image=local.image_gpu }
+        nodepoolcpu   = { type = "c8-60gb", tags = ["node", "pool"], count = local.ncpupool, image=local.image_cpu }
+    }
+  }
+  default_instances = local.default_instances_map[var.cloud_name]
+
+  home_size = local.custom_home_size ? local.custom_home_size : 100
+  project_size = local.custom_project_size ? local.custom_project_size : 100
+  scratch_size = local.custom_scratch_size ? local.custom_scratch_size : 100
+  default_volumes_map = {
+    arbutus = {
+      nfs = {
+        home     = { size = local.home_size }
+        project  = { size = local.project_size }
+        scratch  = { size = local.scratch_size }
+      }
+    }
+    beluga = {
+      nfs = {
+        home     = { size = local.home_size, type = "volumes-ssd"  }
+        project  = { size = local.project_size, type = "volumes-ec"  }
+        scratch  = { size = local.scratch_size, type = "volumes-ec"  }
+      }
+    }
+  }
+  default_volumes = local.default_volumes_map[var.cloud_name]
+
   hieradata = yamlencode(merge(
     var.token_hieradata,
     {
@@ -24,8 +71,6 @@ locals {
     yamldecode(file("../common/config.yaml")),
     yamldecode(file("config.yaml"))
   ))
-  image_cpu = "snapshot-cpunode-2024.1"
-  image_gpu = "snapshot-gpunode-2024.1"
 }
 
 module "openstack" {
@@ -37,7 +82,7 @@ module "openstack" {
   domain       = "calculquebec.cloud"
   image        = "Rocky-8"
 
-  instances = local.instances
+  instances = local.instances ? local.instances : local.default_instances
 
   # var.pool is managed by Slurm through Terraform REST API.
   # To let Slurm manage a type of nodes, add "pool" to its tag list.
@@ -45,7 +90,7 @@ module "openstack" {
   # Refer to Magic Castle Documentation - Enable Magic Castle Autoscaling
   pool = var.pool
 
-  volumes = local.volumes
+  volumes = local.volumes ? local.volumes : local.default_volumes
 
   generate_ssh_key = true
   public_keys = compact(concat(split("\n", file("../common/sshkeys.pub")), ))
