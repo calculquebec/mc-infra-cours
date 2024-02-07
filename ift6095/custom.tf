d9132d45631e45bbea57e0e847a6cb66adb83b5f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-02-07 10:17:06 -0500

    define amount of storage and nodes

--- ift6095/custom.tf
+++ ift6095/custom.tf
@@ -1,15 +1,10 @@
 locals {
-  volumes = {
-    nfs = {
-      home     = { size = 100 }
-      project  = { size = 500 }
-      scratch  = { size = 50 }
-    }
-  }
-  instances = {
-    mgmt   = { type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
-    login  = { type = "p4-6gb-avx2", tags = ["login", "public", "proxy"], count = 1 }
-    nodepool   = { type = "g1-8gb-c4-22gb", tags = ["node", "pool"], count = 10, image=local.image_gpu }
-  }
+  custom_home_size = 100
+  custom_project_size = 500
+  custom_scratch_size = 50
+
+  custom_ngpupool = 10
+  custom_ngpu = 1
+
   name = "ift6095"
 }
