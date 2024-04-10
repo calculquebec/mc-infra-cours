f996beffc86268bb0620244b183257ceb41213bf
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-04-10 08:44:55 -0400

    test config pour l'ecole parallel

---
+++ ecole-p/custom.tf
@@ -0,0 +1,12 @@
+locals {
+  custom = {
+    ngpu = 2
+    ngpupool = 2
+    home_size = 500
+    project_size = 500
+    scratch_size = 10
+    cluster_purpose = "formation"
+  }
+
+  name = "ecole-p"
+}
