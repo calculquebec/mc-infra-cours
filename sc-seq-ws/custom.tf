d6ea5eb5fae4da44a236c75fb350b865df64bbe5
Darcy Quesnel <darcy.quesnel@gmail.com>
2024-06-05 12:36:12 -0400

    Added tags.

--- sc-seq-ws/custom.tf
+++ sc-seq-ws/custom.tf
@@ -1,5 +1,13 @@
 locals {
   custom = {
+    ncpu = 4
+    ncpupool = 4
+    ngpu = 0
+    ngpupool = 0
+    home_size = 100
+    project_size = 100
+    scratch_size = 10
+    cluster_purpose = "formation"
   }
   name = "sc-seq-ws"
 }
