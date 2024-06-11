dc3805a65710ed7dc790fa52433a258624faffbf
Elie Haber <lellohaber@gmail.com>
2024-06-11 13:46:49 -0400

    adding ecolecqq folder

---
+++ ecolecqq/custom.tf
@@ -0,0 +1,13 @@
+locals {
+  custom = {
+    ngpu = 0
+    ngpupool = 0
+    ncpu = 2
+    ncpupool = 5
+    home_size = 100
+    project_size = 50
+    scratch_size = 50
+  }
+
+  name = "ecolecq"
+}
