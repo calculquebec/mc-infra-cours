3efe7ab025a87bb99df61acc2125d2b225a8873b
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-19 17:02:55 -0400

    fix cloudfllare's breakage

--- common/main.tf
+++ common/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.4.0"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.38.0"
+    }
+  }
 }
 
 variable "pool" {
