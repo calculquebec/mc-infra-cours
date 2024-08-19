5fe5a6812a7dce2f23ded28d918bc4d9329be52e
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-19 16:47:04 -0400

    add cluster config for 8inf919

---
+++ 8inf919/custom.tf
@@ -0,0 +1,13 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 15
+    ngpu = 0
+    ngpupool = 0
+    home_size = 100
+    project_size = 1000
+    scratch_size = 500
+  }
+
+  name = "uqac-8inf919"
+}
