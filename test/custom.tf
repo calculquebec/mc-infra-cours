e1de774065c5be211486bdd740c0ddc565ce5ce3
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-19 16:57:15 -0400

    add test directory, to build a test cluster

---
+++ test/custom.tf
@@ -0,0 +1,13 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 1
+    ngpu = 1
+    ngpupool = 1
+    home_size = 100
+    project_size = 100
+    scratch_size = 50
+  }
+
+  name = "test"
+}
