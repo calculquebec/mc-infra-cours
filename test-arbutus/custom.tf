51f75593e527d60c9974c4b27b91cc7289862683
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-20 11:10:55 -0400

    split test directory for arbutus and beluga

---
+++ test-arbutus/custom.tf
@@ -0,0 +1,15 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 1
+    ngpu = 1
+    ngpupool = 1
+    home_size = 100
+    project_size = 100
+    scratch_size = 50
+    image_cpu = "AlmaLinux-9"
+    image_gpu = "AlmaLinux-9"
+  }
+
+  name = "test-mc-infra-cours-arbutus"
+}
