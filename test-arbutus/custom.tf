e1ee59147655eced4f079e83df45756ac4c0deed
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-21 10:46:48 -0400

    changed arbutus images to use the snapshots

--- test-arbutus/custom.tf
+++ test-arbutus/custom.tf
@@ -1,14 +1,14 @@
 locals {
   custom = {
-    ncpu = 1
+    ncpu = 0
     ncpupool = 1
-    ngpu = 2
+    ngpu = 0
     ngpupool = 1
     home_size = 100
     project_size = 100
     scratch_size = 50
-    image_cpu = "Rocky-8"
-    image_gpu = "Rocky-8"
+    image_cpu = "snapshot-cpunode-2024-R810"
+    image_gpu = "snapshot-gpunode-2024-R810"
   }
 
   name = "test-mc-infra-cours-arbutus"
