a803ee6c33a451f2f8f499f483d1dd797fa66f66
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-22 15:34:39 -0400

    use new cloudflare module

--- common/main.tf
+++ common/main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.4.0"
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.38.0"
-    }
-  }
 }
 
 variable "pool" {
