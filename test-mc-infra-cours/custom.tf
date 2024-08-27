3fcfc8f3a263803f9b51f57f846bbb7fa8c99dfd
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-27 15:43:06 -0400

    destroy existing nodes, update image_cpu and image_gpu

--- test-mc-infra-cours/custom.tf
+++ test-mc-infra-cours/custom.tf
@@ -1,12 +1,14 @@
 locals {
   custom = {
-    ncpu = 1
+    ncpu = 0
     ncpupool = 1
-    ngpu = 1
+    ngpu = 0
     ngpupool = 1
     home_size = 100
     project_size = 100
     scratch_size = 50
+    image_cpu = "snapshot-cpunode-2024-R810.3"
+    image_gpu = "snapshot-gpunode-2024-R810.3"
   }
 
   name = "test-mc-infra-cours"
