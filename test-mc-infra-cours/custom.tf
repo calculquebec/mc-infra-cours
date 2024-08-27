0c68692d50ca9bb111e6f8ab51d36f27c4b11321
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-08-27 16:29:17 -0400

    bump snapshot image, boot 1 new node

--- test-mc-infra-cours/custom.tf
+++ test-mc-infra-cours/custom.tf
@@ -1,12 +1,14 @@
 locals {
   custom = {
-    ncpu = 1
+    ncpu = 2
     ncpupool = 1
-    ngpu = 1
+    ngpu = 2
     ngpupool = 1
     home_size = 100
     project_size = 100
     scratch_size = 50
+    image_cpu = "snapshot-cpunode-2024-R810.4"
+    image_gpu = "snapshot-gpunode-2024-R810.4"
   }
 
   name = "test-mc-infra-cours"
