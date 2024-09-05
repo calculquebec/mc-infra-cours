3935af42c110fe3495e117a5d12dc21cadcd17f6
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-05 14:19:09 -0400

    added config for BigBrain workshop

---
+++ bigbrain/custom.tf
@@ -0,0 +1,13 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 15
+    ngpu = 0
+    ngpupool = 0
+    home_size = 100
+    project_size = 500
+    scratch_size = 100
+  }
+
+  name = "bigbrain"
+}
