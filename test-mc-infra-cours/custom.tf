498896cca6bef0b4cceb0528588ec61eb6a5e425
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-20 16:21:38 -0400

    test quotas and slurm config

--- test-mc-infra-cours/custom.tf
+++ test-mc-infra-cours/custom.tf
@@ -4,11 +4,19 @@ locals {
     ncpupool = 1
     ngpu = 0
     ngpupool = 1
-    home_size = 100
-    project_size = 100
-    scratch_size = 50
+#    home_size = 100
+#    project_size = 100
+#    scratch_size = 50
     image_cpu = "snapshot-cpunode-2024-R810.4"
     image_gpu = "snapshot-gpunode-2024-R810.4"
+
+    volumes = {
+      nfs = {
+        home = { size = 100, quota = "1g" }
+        project = { size = 500 }
+	scratch = { size = 400, quota = "4g" }
+      }
+    }
   }
 
   name = "test-mc-infra-cours"
