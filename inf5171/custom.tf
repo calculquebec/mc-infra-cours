31cee1069570168fb655a0240a83cce9f85aa503
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-24 15:08:38 -0400

    add configuration for inf5171 class

---
+++ inf5171/custom.tf
@@ -0,0 +1,13 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 20
+    ngpu = 0
+    ngpupool = 15
+    home_size = 50
+    project_size = 950
+    scratch_size = 50
+  }
+
+  name = "inf1571"
+}
