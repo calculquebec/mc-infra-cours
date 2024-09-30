65ef7b8d9c269c05a05b5b198a71097409fedf40
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-30 15:56:23 -0400

    use snapshot for gpu nodes too

--- mcgill-scs/custom.tf
+++ mcgill-scs/custom.tf
@@ -18,7 +18,7 @@ locals {
     }
 
     image_cpu = "snapshot-cpunode-2024-R810.5"
-    image_gpu = "Rocky-8" #snapshot-gpunode-2024-R810.4"
+    image_gpu = "snapshot-gpunode-2024-R810.5"
 
     config_version = "ef3e870"
   }
