a9b217b2a2b6f4443066e9a3539f442183cd58f5
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-02 10:45:50 -0400

    add MIG back with drivers 560, from GPUs that have MIG disabled

--- mcgill-scs/custom.tf
+++ mcgill-scs/custom.tf
@@ -18,9 +18,9 @@ locals {
     }
 
     mig = {
-      gpupool12 = null #{ "1g.5gb" = 7 }
-      gpupool16 = null #{ "3g.20gb" = 1, "4g.20gb" = 1 }
-      gpupool80 = null #{ "1g.10gb" = 1, "2g.20gb" = 3 }
+      gpupool16 = { "1g.5gb" = 7 }
+      gpupool12 = { "3g.20gb" = 1, "4g.20gb" = 1 }
+      gpupool80 = { "1g.10gb" = 1, "2g.20gb" = 3 }
     }
 
     image_cpu = "snapshot-cpunode-2024-R810.5"
