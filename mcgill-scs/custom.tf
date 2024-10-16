201d2ba57e6bfe11bfbe6dd96eb1e2841b5b0897
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-16 11:25:29 -0400

    change mig config to use only 3g.20gb, and bump puppet-mc commit

--- mcgill-scs/custom.tf
+++ mcgill-scs/custom.tf
@@ -19,14 +19,14 @@ locals {
 
     mig = {
       gpupool16 = { "1g.5gb" = 7 }
-      gpupool12 = { "3g.20gb" = 1, "4g.20gb" = 1 }
+      gpupool12 = { "3g.20gb" = 2 }
       gpupool80 = { "2g.20gb" = 3 }
     }
 
     image_cpu = "snapshot-cpunode-2024-R810.5"
     image_gpu = "snapshot-gpunode-2024-R810.5"
 
-    config_version = "2972853"
+    config_version = "dc6b37f4d2c077a37d88bf4862ba57a09eed7213"
   }
 
   name = "mcgill-scs"
