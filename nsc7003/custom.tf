84cfa0429b95eb7acf322edacb59bedb12fb0313
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-01-06 15:50:35 -0500

    add initial configuration for nsc7003

---
+++ nsc7003/custom.tf
@@ -0,0 +1,23 @@
+locals {
+  custom = {
+    nnodes = {
+      gpu = 0
+      gpupool = 4
+      cpupool = 2
+    }
+    home_size = 50
+    project_size = 500
+    scratch_size = 50
+
+    user_quotas = {
+      home = "5g"
+      project = "50g"
+      scratch = "5g"
+    }
+
+    mig = {
+      gpupool = { "2g.10gb" = 3 }
+    }
+  }
+  name = "nsc7003"
+}
