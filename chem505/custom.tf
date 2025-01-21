22c4d0e64da5ce77b81c32f708841822caaa5dc8
Bart Oldeman <bart.oldeman@calculquebec.ca>
2025-01-21 13:56:21 +0000

    Bump quotas to 10G on chem505 virtual cluster

--- chem505/custom.tf
+++ chem505/custom.tf
@@ -5,6 +5,12 @@ locals {
       cpupool = 8
     }
 
+    user_quotas = {
+      home = "10g"
+      project = "10g"
+      scratch = "10g"
+    }
+
     instances_type_map = {
       beluga = {
         cpu = "c2-7.5gb"
