917a8bfdb2f5564e15ffe885a92a6db06fba3760
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-03-04 17:08:52 -0500

    recovered deleted files from history

---
+++ bigbrain/custom.tf
@@ -0,0 +1,15 @@
+locals {
+  custom = {
+    ncpu = 1
+    ncpupool = 3
+    ngpu = 0
+    ngpupool = 0
+    home_size = 100
+    project_size = 500
+    scratch_size = 100
+
+    cluster_purpose = "custom_event"
+  }
+
+  name = "bigbrain"
+}
---
+++ cdsi-intro-hpc/custom.tf
@@ -0,0 +1,8 @@
+locals {
+  name = "cdsi-intro-hpc"
+
+  custom = {
+    ncpu = 2
+    nnode_compute = 2
+    }
+}
