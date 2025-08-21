a1601349f06e1cf426a671ed516b41ed2802ce1f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-08-21 13:04:06 -0400

    bump puppet-mc

--- common/common.tf
+++ common/common.tf
@@ -70,7 +70,7 @@ locals {
 
     cluster_purpose = "cours_academiques"
     config_git_url = "https://github.com/calculquebec/puppet-magic_castle_formation.git"
-    config_version = "8fb97c4"
+    config_version = "3c0ca77"
 
     instances_type_map = {
       arbutus = {
