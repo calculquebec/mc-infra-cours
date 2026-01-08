cf4b20bb1cdaae0be27d1610562ba546cf40cdbe
Helene Gingras <helene.gingras@calculquebec.ca>
2026-01-08 10:37:12 -0500

    Ajout fichiers pour grappe ift3710

---
+++ ift3710/main.tf
@@ -0,0 +1,23 @@
+locals {
+  custom = {
+    nnodes = {
+      cpupool = 15
+      gpupool = 15
+    }
+    home_size = 250
+    project_size = 250
+    scratch_size = 250
+
+    user_quotas = {
+      home = "5g"
+      project = "5g"
+      scratch = "5g"
+    }
+
+    mig = {
+      gpupool = { "2g.10gb" = 3 }
+    }
+  }
+
+  name = "ift704"
+}
