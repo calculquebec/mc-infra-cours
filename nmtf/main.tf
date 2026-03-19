e5e66ef3755c165328a3e574a054399a2917d72a
Helene Gingras <helene.gingras@calculquebec.ca>
2026-03-19 07:48:51 -0400

    Modification de la config pour grappe de test

--- nmtf/main.tf
+++ nmtf/main.tf
@@ -1,11 +1,11 @@
 locals {
   custom = {
     nnodes = {
-      cpupool = 22 
+      cpupool = 1 
     }
     home_size = 50
-    project_size = 110
-    scratch_size = 250
+    project_size = 50
+    scratch_size = 50
 
     user_quotas = {
       home = "2g"
